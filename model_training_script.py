f2338c6134516289912d35cc4a6af910419e4458
Joseph Thi <josephthi888@gmail.com>
2026-01-03 23:34:25 -0800

    added more columns for calculating game stats

--- model_training_script.py
+++ model_training_script.py
@@ -1,5 +1,6 @@
 from xgboost import XGBClassifier
 import numpy as np
+from pathlib import Path
 import pandas as pd
 from sklearn.frozen import FrozenEstimator
 from sklearn.calibration import CalibratedClassifierCV
@@ -22,13 +23,49 @@ def sort_by_date(df):
     return df.sort_values(by='GAME_DATE')
 
 
-def get_csv_info(directory: str) -> pd.DataFrame:
-    nba_player_data_2025_26 = pd.read_csv('out/player_stats_2025-26.csv')
-    nba_player_data_2024_25 = pd.read_csv('out/player_stats_2024-25.csv')
-    nba_players = pd.read_csv('out/players.csv')
-    nba_teams = pd.read_csv('out/teams.csv')
-    nba_season_games_2024_25 = pd.read_csv('out/season_games_2024-25.csv')
-    nba_season_games_2025_26 = pd.read_csv('out/season_games_2025-26.csv')
+def get_batch_csv_info(directory: str) -> list[pd.DataFrame]:
+    csv_info_list = []
+    for file in Path(directory).glob("*.csv"):
+        csv_info_list.append(pd.read_csv(file))
+    return csv_info_list
+
+
+def modify_data_for_model(data: pd.DataFrame) -> pd.DataFrame:
+    # Example modification: Fill NaN values with 0
+    data = data.fillna(0)
+    # Add more feature engineering steps as needed
+
+    # Add IS_HOME column
+    data["IS_HOME"] = data["MATCHUP"].apply(is_home)
+
+    # Sort by TEAM_ID and GAME_DATE
+    sort_by_list = ["TEAM_ID", "GAME_DATE"]
+    data = data.sort_values(by=sort_by_list, inplace=True)
+
+    # Change WL to binary
+    data["WL"] = data["MATCHUP"].apply(is_win)
+
+    #Effective field goal percentage
+    data["EFG"] = (data["FGM"] + 0.5 * data["FG3M"]) / data["FGA"]
+
+    #True shooting percentage
+    data["TS"] = data["PTS"] / (2 * (data["FGA"] + 0.44 * data["FTA"]))
+
+    #Assist to turnover ratio
+    data["ast_tov"] = data["AST"] / (data["TOV"] + 1e-8)  # Add small value to avoid division by zero)
+
+    #Turnover rate
+    data["tov_rate"] = data["TOV"] / (data["FGA"] + 0.44 * data["FTA"] + 1e-8) # Add small value to avoid division by zero
+
+    #offensive rebound rate
+    data["oreb_rate"] = data["OREB"] / (data["OREB"] + data["DREB"] + 1e-8) # Add small value to avoid division by zero
+
+    #stealing and blocks
+    data["stocks"] = data["STL"] + data["BLK"]
+
+    #Personal fouls rate
+    data["pf_rate"] = data["PF"] / (data["MIN"] + 1e-8) # Add small value to avoid division by zero
+
 
 
 if __name__ == "__main__":
