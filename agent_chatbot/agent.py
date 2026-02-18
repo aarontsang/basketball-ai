0e0cf16d8d8246d5945c476890be9a37ed64a3a9
Joseph Thi <josephthi888@gmail.com>
2026-02-17 16:04:19 -0800

    enabled agent to get directed to player stats function

--- agent_chatbot/agent.py
+++ agent_chatbot/agent.py
@@ -1,19 +1,20 @@
 from llama_index.core.tools import FunctionTool
+from stats_query_handler import get_player_career_stats, get_team_stats
 
 stats_tool = FunctionTool.from_defaults(
-    fn=get_player_stats,
+    fn=get_player_career_stats,
     name="nba_stats_tool",
     description="Use this tool to get real NBA player or team statistics."
 )
 
-prediction_tool = FunctionTool.from_defaults(
-    fn=predict_game,
-    name="xgboost_prediction_tool",
-    description="Use this tool when user asks to predict game outcomes."
-)
+# prediction_tool = FunctionTool.from_defaults(
+#     fn=predict_game,
+#     name="xgboost_prediction_tool",
+#     description="Use this tool when user asks to predict game outcomes."
+# )
 
-rag_tool = FunctionTool.from_defaults(
-    fn=rag_query,
-    name="wikipedia_basketball_tool",
-    description="Use this tool for basketball history or contextual questions."
-)
+# rag_tool = FunctionTool.from_defaults(
+#     fn=rag_query,
+#     name="wikipedia_basketball_tool",
+#     description="Use this tool for basketball history or contextual questions."
+# )
